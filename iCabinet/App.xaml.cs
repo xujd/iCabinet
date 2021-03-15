172ca48c61442c06ff7329577adc5d6e7177e087
xujundong_ext <xu.jd@inspur.com>
2021-03-15 14:41:51 +0800

    init project

---
+++ iCabinet/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace iCabinet
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static System.Threading.Mutex mutex;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            mutex = new System.Threading.Mutex(true, "OnlyRun_CRNS");
+            if (mutex.WaitOne(0, false))
+            {
+                base.OnStartup(e);
+            }
+            else
+            {
+                MessageBox.Show("程序已经在运行！", "提示");
+                this.Shutdown();
+            }
+        }
+    }
+}
